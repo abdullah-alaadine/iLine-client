83419be689c7248e39ee61de205fa364f6ef6093
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-21 22:54:56 +0300

    feat(SearchResult): add handleNewChat function
    Add a new function handleNewChat to the SearchResult.js file, which sends a POST request to create a new chat with the search result user's ID as a member, and updates the chats state with the new chat. The function also sorts the updated chats state based on the updatedAt property.

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,28 +1,34 @@
+import { useSelector } from "react-redux";
+import { createChat } from "../api/chatsAPI";
 import Profile from "../assets/profileImg.webp";
 
-const SearchResult = ({searchResult, setSearch}) => {
-
-    const handleNewChat = async () => {
-        setSearch(false)
+const SearchResult = ({ searchResult, setSearch, setChats, chats }) => {
+  const { token } = useSelector((state) => state.authReducer);
+  const handleNewChat = async () => {
+    setSearch(false);
+    try {
+      const {data} = await createChat({ members: [searchResult._id] }, token);
+      setChats([...chats, data].sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt)));
+    } catch (error) {
+        console.log(error)
     }
+  };
 
   return (
     <div
-        onClick={handleNewChat}
-        className="flex gap-8 p-2 items-center border-b border-slate-500 rounded-lg hover:bg-slate-500 hover:cursor-pointer"
-      >
-        <img
-          className="w-12 sm:w-14 md:w-15 rounded-full"
-          src={searchResult.profilePic ?? Profile}
-          alt=""
-        />
-        <p className="text-base text-center w-full self-center">
-          {searchResult.firstName} {searchResult.lastName}
-        </p>
-        
-      </div>
-    );
-  
-}
+      onClick={handleNewChat}
+      className="flex gap-8 p-2 items-center border-b border-slate-500 rounded-lg hover:bg-slate-500 hover:cursor-pointer"
+    >
+      <img
+        className="w-12 sm:w-14 md:w-15 rounded-full"
+        src={searchResult.profilePic ?? Profile}
+        alt=""
+      />
+      <p className="text-base text-center w-full self-center">
+        {searchResult.firstName} {searchResult.lastName}
+      </p>
+    </div>
+  );
+};
 
-export default SearchResult
+export default SearchResult;
