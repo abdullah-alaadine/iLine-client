a1339f2d5e9539c7046149f21953eaa874cae577
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-22 04:00:10 +0300

    add getUserChat function

--- src/utils/checkUserExistence.js
+++ src/utils/checkUserExistence.js
@@ -1,7 +1,15 @@
 export const userExists = (arr, userObj) => {
-    for(let i = 0; i< arr.length; i++ ){
-        if(arr[i]._id === userObj._id){
-            return userObj
-        }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]._id === userObj._id) {
+      return userObj;
     }
-}
+  }
+};
+
+export const getUserChat = (arr, userObj) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]._id === userObj._id) {
+      return arr[i];
+    }
+  }
+};
