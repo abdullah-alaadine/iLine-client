454ef2a7cd655799b0d3eb6a7ae0e782158db589
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-22 09:52:59 +0300

    fix bug: move return keyword outside the loop

--- src/utils/checkChatExistence.js
+++ src/utils/checkChatExistence.js
@@ -1,11 +1,11 @@
 export const groupChatExists = (arr, chatName) => {
+  const groupChats = [];
   for (let i = 0; i < arr.length; i++) {
-    const groupChats = [];
     if (arr[i].isGroup) {
       if (arr[i].name.includes(chatName)) {
         groupChats.push(arr[i]);
       }
     }
-    return groupChats;
   }
+  return groupChats;
 };
