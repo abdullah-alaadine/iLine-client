d3a47ffffef1dd4dc7da01995ea4c2ff4c389bc8
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-23 06:06:46 +0300

    add an expiry date for the token

--- src/store.js
+++ src/store.js
@@ -1,23 +1,35 @@
-import {legacy_createStore} from 'redux'
-import allReducers from './reducers';
+import { legacy_createStore } from "redux";
+import allReducers from "./reducers";
 
 function saveToLocalStorage(store) {
-    try {
-        window.localStorage.setItem('store', JSON.stringify(store));
-    } catch(e) {
-        console.log(e);
+  try {
+    if (localStorage.getItem("expirationDate")) {
+      window.localStorage.setItem("store", JSON.stringify(store));
+    } else {
+      window.localStorage.setItem("store", JSON.stringify(store));
+      const expirationDate = 24 * 60 * 60 * 1000;
+      window.localStorage.setItem("expirationDate", expirationDate);
+      setTimeout(() => {
+        localStorage.removeItem("expirationDate");
+        localStorage.removeItem("store");
+        alert("Session is expired! Please login again");
+        location.reload();
+      }, expirationDate);
     }
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 function loadFromLocalStorage() {
-    try {
-        const serializedStore = window.localStorage.getItem('store');
-        if(!serializedStore) return undefined;
-        return JSON.parse(serializedStore);
-    } catch(e) {
-        console.log(e);
-        return undefined;
-    }
+  try {
+    const serializedStore = window.localStorage.getItem("store");
+    if (!serializedStore) return undefined;
+    return JSON.parse(serializedStore);
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
 }
 
 const persistedState = loadFromLocalStorage();
@@ -26,4 +38,4 @@ const store = legacy_createStore(allReducers, persistedState);
 
 store.subscribe(() => saveToLocalStorage(store.getState()));
 
-export default store
+export default store;
