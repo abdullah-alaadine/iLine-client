54b470cd56957eea491c486b5d02c9a5edc9ff2d
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-23 08:46:51 +0300

    create submit handlers

--- src/components/GroupList.jsx
+++ src/components/GroupList.jsx
@@ -1,28 +1,39 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
-import { createChat } from "../api/chatsAPI";
+import { createChat, updateChat } from "../api/chatsAPI";
+import { useSelector } from "react-redux";
 
-const GroupList = ({ groupMembers, chats, setChats }) => {
+const GroupList = ({ chat, chats, setChats, setNewGroup }) => {
+  const { token } = useSelector((state) => state.authReducer);
   const nameRef = useRef(null);
-  const [group, setGroup] = useState([
-    { id: 1, firstName: "jalal", lastName: "jamal" },
-  ]);
+  const [group, setGroup] = useState(chat?.members ?? []);
 
   const handleCreateGroupChat = async () => {
     try {
-      const { data } = await createChat(group);
+      const { data } = await createChat(group, token);
       setChats(
         [...chats, data].sort(
           (a, b) => new Date(b.updatedAt) - new Date(a.updatedAt)
         )
       );
+      setNewGroup(false)
     } catch (error) {
       console.log(error);
     }
   };
 
-  const handleEditGroupChat = async () => {};
+  const handleEditGroupChat = async () => {
+    try {
+      const { data } = await updateChat(chat._id, {
+        name: nameRef.current.value,
+        members: group,
+      }, token);
+      setChats(chats.map(obj => data._id === obj._id ? {...data} : obj))
+    } catch (error) {
+        console.log(error)
+    }
+  };
 
   return (
     <div className="flex flex-col gap-2 items-center">
@@ -34,7 +45,7 @@ const GroupList = ({ groupMembers, chats, setChats }) => {
       />
       <div className="flex flex-wrap">
         {group.map((member) => (
-          <div className="flex gap-8 p-2 items-center border-b border-r border-slate-500 rounded-lg hover:bg-slate-500">
+          <div key={member.id} className="flex gap-8 p-2 items-center border-b border-r border-slate-500 rounded-lg hover:bg-slate-500">
             <p className="text-center text-xs md:text-sm w-full self-center">
               {member.firstName} {member.lastName}
             </p>
@@ -53,7 +64,7 @@ const GroupList = ({ groupMembers, chats, setChats }) => {
       />
       <button
         onClick={
-          groupMembers.length ? handleEditGroupChat : handleCreateGroupChat
+          chat?.members.length ? handleEditGroupChat : handleCreateGroupChat
         }
         className=" bg-slate-700 w-fit py-1 px-2 rounded-lg text-slate-100 hover:bg-slate-100 hover:text-slate-700"
       >
