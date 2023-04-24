5756d601b16b0bae88889bd7974c650e9dbc8a49
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-24 11:23:49 +0300

    only groupAdmin can update group info

--- src/components/GroupMembersCard.jsx
+++ src/components/GroupMembersCard.jsx
@@ -1,13 +1,28 @@
+import { useSelector } from "react-redux";
 import GroupIcon from "../assets/groupIcon.jpg";
 import GroupList from "./GroupList";
 
-const GroupMembersCard = ({chat, chats, setChats, setGroupCard}) => {
-    return (
-      <div className="absolute overflow-y-scroll w-full flex flex-col items-center gap-2 bg-slate-600 p-4 m-auto" onClick={(e) => e.stopPropagation()}>
-        <img src={chat.profilePic ?? GroupIcon} className="w-40 rounded-full" />
-        <GroupList chat={chat} chats={chats} setChats={setChats} setGroupCard={setGroupCard}/>
-      </div>
-    );
+const GroupMembersCard = ({ chat, chats, setChats, setGroupCard }) => {
+  const { _id } = useSelector((state) => state.authReducer.user);
+
+  return (
+    <div
+      className="absolute overflow-y-scroll w-full flex flex-col items-center gap-2 bg-slate-600 p-4 m-auto"
+      onClick={(e) => e.stopPropagation()}
+    >
+      <img src={chat.profilePic ?? GroupIcon} className="w-40 rounded-full" />
+      {_id === chat.groupAdmin ? (
+        <GroupList
+          chat={chat}
+          chats={chats}
+          setChats={setChats}
+          setGroupCard={setGroupCard}
+        />
+      ) : (
+        ""
+      )}
+    </div>
+  );
 };
 
 export default GroupMembersCard;
