33ed010ca678989495da6108c031365857c3b9a3
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-24 07:22:42 +0300

    remove divs around BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,20 @@ function App() {
   const { user } = useSelector((state) => state.authReducer);
 
   return (
-    <div className="scroll bg-slate-400 w-full flex flex-col gap-5">
-      <div className="relative w-full bg-slate-100 h-screen border-2 mx-auto">
-        <BrowserRouter>
-          <Routes>
-            <Route
-              path="/"
-              element={user ? <Home /> : <Navigate to="/login" />}
-            />
-            <Route
-              path="/login"
-              element={user ? <Navigate to="/" /> : <LoginForm />}
-            />
-            <Route
-              path="/signup"
-              element={user ? <Navigate to="/" /> : <SignupForm />}
-            />
-            <Route path="*" element={<h1>404 Not Found!</h1>} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+        <Route
+          path="/login"
+          element={user ? <Navigate to="/" /> : <LoginForm />}
+        />
+        <Route
+          path="/signup"
+          element={user ? <Navigate to="/" /> : <SignupForm />}
+        />
+        <Route path="*" element={<h1>404 Not Found!</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
