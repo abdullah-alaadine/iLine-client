3120ae8e7fba205bcdba5cbf267f4e69f25455c1
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-25 11:18:45 +0300

    add 'UPDATE_SUCCESS' action to authReducer

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -2,6 +2,8 @@ const authReducer = (state = { user: null, token: ''}, action) => {
     switch(action.type){
         case "AUTH_SUCCESS":
             return {user: action.payload.user,  token: action.payload.token}
+        case "UPDATE_SUCCESS":
+            return {...state, user: action.payload}
         case "Logout":
             return null
         default:
