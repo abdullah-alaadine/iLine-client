ec80290c6f48655cab23ff9eab87db017d844dd5
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-26 03:52:08 +0300

    update the parameters to take user or chat ID

--- src/utils/uploadToFirebaseStorage.js
+++ src/utils/uploadToFirebaseStorage.js
@@ -1,11 +1,12 @@
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { storage } from "../firebase";
 
-export const uploadImage = async (userId, img) => {
+export const uploadImage = async (userOrChatId, img) => {
     try {
-      const imgRef = ref(storage, `images/${userId}`);
+      const imgRef = ref(storage, `images/${userOrChatId}`);
       const snapShot = await uploadBytesResumable(imgRef, img);
       const url = await getDownloadURL(imgRef);
+      console.log(url)
       return url;
     } catch (error) {
       throw new Error(error.message);
