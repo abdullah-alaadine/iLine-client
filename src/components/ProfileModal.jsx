72305d9cd9b55bb64ee803f2ccedbb2c99267abb
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-26 07:19:40 +0300

    add logout button to Profile Modal

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -42,11 +42,22 @@ const ProfileModal = ({ user, setProfileModal }) => {
     }
   };
 
+  const handleLogout = () => {
+    localStorage.clear();
+    location.reload();
+  };
+
   return (
     <div
       onClick={(e) => e.stopPropagation()}
       className="absolute border-2 border-slate-300 bg-slate-500 rounded-lg w-[92%] md:w-[60%] gap-2 flex flex-col items-center p-2 mt-2"
-    >
+    > 
+      <button
+        onClick={handleLogout}
+        className="disabled:hover:cursor-auto bg-slate-700 w-fit py-1 px-2 rounded-lg text-slate-100 hover:bg-slate-100 hover:text-slate-700"
+      >
+        Logout
+      </button>
       <div className="relative">
         <FontAwesomeIcon
           onClick={() => imageRef.current.click()}
