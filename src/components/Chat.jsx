6bf08c3b1a75d731dced5aafaceef77890cbae54
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-28 05:25:23 +0300

    add options Icon in Chat component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,18 +3,22 @@ import GroupIcon from "../assets/groupIcon.jpg";
 import { format } from "timeago.js";
 import { useEffect } from "react";
 import { socket } from "../utils/initializeSocketConnection";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
 
 const Chat = ({ chat, setChat, thisChat }) => {
-  
   useEffect(() => {
-    socket.emit("joinRoom", {chatId: chat._id});
-  }, [])
+    socket.emit("joinRoom", { chatId: chat._id });
+  }, []);
 
   if (!chat.isGroup) {
     return (
       <div
         onClick={() => setChat(chat)}
-        className={"flex gap-8 p-2 w-full items-center border-b border-slate-500 rounded-lg hover:bg-slate-500 hover:cursor-pointer" + (thisChat === chat ? ' bg-slate-500': '')}
+        className={
+          "flex gap-8 p-2 w-full items-center border-b border-slate-500 rounded-lg hover:bg-slate-500 hover:cursor-pointer" +
+          (thisChat === chat ? " bg-slate-500" : "")
+        }
       >
         <img
           className="w-10 sm:w-12 md:w-10 border-2 border-slate-800 lg:w-12 rounded-full"
@@ -28,6 +32,16 @@ const Chat = ({ chat, setChat, thisChat }) => {
           <p className="text-[8px] w-8 md:text-[9px] lg:text-[10px] text-slate-700">
             {format(chat.updatedAt).slice(0, format(chat.updatedAt).length - 4)}
           </p>
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faEllipsisH}
+              className="text-slate-900 rounded-full hover:bg-slate-300 p-1"
+            />
+          </button>
         </div>
       </div>
     );
@@ -35,18 +49,33 @@ const Chat = ({ chat, setChat, thisChat }) => {
     return (
       <div
         onClick={() => setChat(chat)}
-        className={"flex gap-8 p-2 border-slate-500 items-center border-b rounded-lg hover:bg-slate-500 hover:cursor-pointer" + (thisChat === chat ? ' bg-slate-500': '')}
+        className={
+          "flex gap-8 p-2 border-slate-500 items-center border-b rounded-lg hover:bg-slate-500 hover:cursor-pointer" +
+          (thisChat === chat ? " bg-slate-500" : "")
+        }
       >
         <img
           className="w-10 sm:w-12 md:w-11 lg:w-12 border-2 border-slate-800 rounded-full"
           src={chat.groupPicture ?? GroupIcon}
           alt=""
         />
-        <p className="text-xs md:text-sm text-slate-900 text-center w-full self-center">{chat.name}</p>
+        <p className="text-xs md:text-sm text-slate-900 text-center w-full self-center">
+          {chat.name}
+        </p>
         <div>
           <p className="text-[8px] w-8 md:text-[9px] lg:text-[10px] text-slate-700">
             {format(chat.updatedAt).slice(0, format(chat.updatedAt).length - 4)}
           </p>
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faEllipsisH}
+              className="text-slate-900 rounded-full hover:bg-slate-300 p-1"
+            />
+          </button>
         </div>
       </div>
     );
